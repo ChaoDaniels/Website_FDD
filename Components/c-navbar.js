069a15d901908e943552afc593d5f05a21254eba
Chao Daniels <cnjdaniels@student.che.nl>
2024-12-13 12:53:25 +0100

    git commit

--- Components/c-navbar.js
+++ Components/c-navbar.js
@@ -5,20 +5,20 @@ class NavHeaderElement extends HTMLElement {
 
     connectedCallback() {
         this.innerHTML = `
-            <nav class="navbar">
+            <div class="navbar">
                 <div class="header">
-                    <i class="fa fa-bars" style="font-size:36px;"></i>
+                    <button class="changebutton" onclick="sidebaropen()"><i class="fa fa-bars fa-2x"></i></button>
                     <div class="HamburgerReplacement"></div>
                         <nav>
                             <ul>
-                                <li><a href="/index.html">Home</a></li>
-                                <li><a href="Lesstof/lesstof.html">Lesstof</a></li>
+                                <li><a href="./index.html">Home</a></li>
                                 <li><h1>Website FDD</h1></li>
                                 <li><a href="zoeken.html" class="split">Zoeken</a></li>
                                 <li><a href="/Over_mij/Over_mij.html" class="split">Over mij</a></li>
                             </ul>
                         </nav>
                 </div>
+            </div>
         `;
     }
 
