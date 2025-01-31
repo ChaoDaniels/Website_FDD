f486dde8fac2517f2133672b992394388bb2a526
Chao <cnjdaniels@student.che.nl>
2025-01-31 10:17:12 +0100

    git commit usability test + fixed contrast

--- Components/c-navbar.js
+++ Components/c-navbar.js
@@ -20,7 +20,7 @@ class NavHeaderElement extends HTMLElement {
                     <button class="changebutton" onclick="sidebaropen()"><i class="fa fa-bars fa-2x"></i></button>  
                     <nav>
                         <a href="${base}/index.html">Home</a>
-                        <button id="contrast-toggle" class="button-white">Toggle Contrast</button>
+                        <button id="contrast-toggle" class="button-white">Contrast Modus</button>
                         <h1>Website FDD</h1>
                         <a href="${base}/zoeken.html">Zoeken</a>
                         <a href="${base}/Over_mij/Contact/contact.html">Contact</a>
@@ -52,13 +52,11 @@ class SideBarElement extends HTMLElement {
                 <button class="changebutton" onclick="sidebarclose()"><i class="fas fa-xmark" style="font-size: 56px;"></i></button>
             </div>
             <div class="menu">
-                <a class="sidebar-btn" href="${base}/zoeken.html" class="split">Zoeken</a>
                 <a class="sidebar-btn" href="${base}/Lesstof/lesstof.html">Lesstof</a>
                 <a class="sidebar-btn" href="${base}/Lesstof/Design/design.html">Design</a>
                 <a class="sidebar-btn" href="${base}/Lesstof/Development/development.html">Development</a>
                 <a class="sidebar-btn" href="${base}/Lesstof/Usability/usability.html">Usability</a>
                 <a class="sidebar-btn" href="${base}/Over_mij/Over_mij.html" class="split">Over mij</a>
-                <a class="sidebar-btn" href="${base}/Over_mij/Contact/contact.html" class="split">Contact</a>
             </div>
           </div>
         `;
